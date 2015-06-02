14a34ee6e1133ff683dd0c34626a8c7faffc841b
Pedro Mass <pedro.j.mass@gmail.com>
2015-06-02 17:47:00 -0400

    Added auth to some of the routes

--- app/routes/categories.server.routes.js
+++ app/routes/categories.server.routes.js
@@ -2,17 +2,18 @@
 
 module.exports = function(app) {
     var categories = require('../../app/controllers/categories.server.controller');
+    var users = require('../../app/controllers/users.server.controller');
 
 	app.route('/categories')
         .get(categories.list)
-        .post(categories.create)
+        .post(users.requiresLogin, categories.create)
     ;
 
     // the categoryId param is added to the params object for the request
     app.route('/categories/:categoryId')
         .get(categories.read)
-        .put(categories.update)
-        .delete(categories.delete)
+        .put(users.requiresLogin, categories.update)
+        .delete(users.requiresLogin, categories.delete)
     ;
 
     app.param('categoryId', categories.categoryByID);
